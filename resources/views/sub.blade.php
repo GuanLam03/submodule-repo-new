892af4348a355e80849847184d37859811d35e7c
Guan lam <hgledgetech@gmail.com>
2025-07-24 15:19:45 +0800

    edit sub blade

--- resources/views/sub.blade.php
+++ resources/views/sub.blade.php
@@ -10,8 +10,8 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
        <ul>
-        <li>Main</li>
-        <li>Sub</li>
+        <li>hello</li>
+        <li>welcome</li>
        </ul>
     </body>
 </html>
